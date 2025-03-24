97a1b6a58c4020fdb1afef17683f9f536da9d4dd
zdvps <zn-dv@outlook.com>
2025-03-24 10:20:22 -0300

    desenvolvendo o infix

---
+++ infixcalc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Autor: Zenio Almeida zn-dv@outlook.com
+# Atualizado: 21/03/2025
+
+"""Calculadora infix.
+
+Funcionamento:
+
+[operação] [n1] [n2]
+
+Operações:
+sum -> +
+sub -> -
+mul -> *
+div -> /
+
+Uso:
+$ infixcalc.py sum 5 2
+7
+
+$ infixcalc.py mul 10 5
+50
+
+$ infixcalc.py
+operação: sum
+n1: 5
+n2: 4
+9
+
+Os resultados serão salvos em `infixcalc.log`
+"""
+__version__ = "0.1.0"
+
+import sys
+
+arguments = sys.argv[1:]
+
+if not arguments:
+    operation = input("operação:")
+    n1 = input("n1:")
+    n2 = input("n2:")
+    arguments = [operation, n1, n2]
+elif len(arguments) != 3:
+    print("Número de argumentos inválidos")
+    print("ex: 'sum 5 5'")
+    sys.exit(1)
+
